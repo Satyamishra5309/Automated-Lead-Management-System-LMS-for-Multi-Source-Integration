361d5eee16093455f20b71751fccf435fd5e2948
satyamishra5309 <satyamishra5309@gmail.com>
2026-06-27 00:49:56 +0530

    second commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+function App() {
+  return (
+    <div className="bg-primary min-h-screen flex justify-center items-center">
+
+      <h1 className="text-white font-primary bg-danger text-4xl">
+
+        LMS Project Started
+
+      </h1>
+
+    </div>
+  )
+}
+
+export default App;
